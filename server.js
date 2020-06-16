f1512150953c20d83ca417e069d38db5363ed8e7
tend2infinity <somu12.ss@gmail.com>
2020-06-16 18:55:29 +0530

    fixed pareser issue

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-Parser');
+const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const knex = require('knex');
 const cors = require('cors');
